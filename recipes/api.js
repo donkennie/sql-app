a41ee59665947c7c350be88aca0e702ff39c57a8
donkennie <ajeigbekehinde160@gmail.com>
2023-08-01 21:46:28 +0100

    changes

--- recipes/api.js
+++ recipes/api.js
@@ -22,9 +22,31 @@ router.get("/detail", (_, res) =>
  */
 
 // connect to postgres
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recipeguru",
+  password: "lol",
+  port: 5432,
+});
+
 
 router.get("/search", async function (req, res) {
   console.log("search recipes");
+
+  const {rows} = await pool.query(
+    `SELECT DISTINCT ON (r.recipe_id)
+      r.recipe_id, r.title, COALESCE(rp.url, 'default.jpg') AS url
+    FROM 
+      recipes r
+    LEFT JOIN 
+      recipes_photoss rp
+    ON 
+      r.recipe_id = rp.recipe_id`
+  );
+
+  res.json({rows})
 
   // return recipe_id, title, and the first photo as url
   //
@@ -36,6 +58,57 @@ router.get("/search", async function (req, res) {
 router.get("/get", async (req, res) => {
   const recipeId = req.query.id ? +req.query.id : 1;
   console.log("recipe get", recipeId);
+  
+  const ingredientsPromise = pool.query(
+    `
+    SELECT
+      i.title AS ingredient_title,
+      i.image AS ingredient_image,
+      i.type AS ingredient_type
+    FROM
+      recipe_ingredients ri
+
+    INNER JOIN
+      ingredients i
+    ON
+      i.id = ri.ingredient_id
+
+    WHERE
+      ri.recipe_id = $1;
+  `,
+    [recipeId]
+  );
+
+  const photosPromise = pool.query(
+    `
+    SELECT 
+      r.title, r.body, COALESCE(rp.url, 'default.jpg') AS url
+    FROM 
+      recipes r
+
+    LEFT JOIN
+      recipes_photos rp
+    ON
+      rp.recipe_id = r.recipe_id
+
+    WHERE 
+      r.recipe_id = $1;
+  `,
+    [recipeId]
+  );
+
+  const [{ rows: photosRows }, { rows: ingredientsRows }] = await Promise.all([
+    photosPromise,
+    ingredientsPromise,
+  ]);
+
+  res.json({
+    ingredients: ingredientsRows,
+    photos: photosRows.map((photo) => photo.url),
+    title: photosRows[0].title,
+    body: photosRows[0].body,
+  });
+});
 
   // return all ingredient rows as ingredients
   //    name the ingredient image `ingredient_image`
@@ -51,8 +124,6 @@ router.get("/get", async (req, res) => {
   // return the body as body
   // if no row[0] has no photo, return it as default.jpg
 
-  res.status(501).json({ status: "not implemented" });
-});
 /**
  * Student code ends here
  */
